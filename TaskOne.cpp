3a49c901e801a6d6ee517cea31a1927be9298f3c
OlenaSekreta <72521407+OlenaSekreta@users.noreply.github.com>
2021-03-11 13:07:05 +0200

    Add files via upload

---
+++ TaskOne.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+//1 + (x/1) + (x^2/2!) + ... + (x^n/n!)
+//n/2^n : 1/2 + 2/4 + 3/8....
+int main1(){
+	float n, s = 0, ryad;
+	cout << "enter n: ";
+	cin >> n;
+	for (int i = 1; i < n; i++) {
+		for (n; n < 10; n++) {
+			ryad = 
+			s = n / pow(2, n);
+			cout << s << endl;
+		}
+	}
+
+}
