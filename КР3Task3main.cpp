03849c13cb80e9a7dfa85f8edb87284fbace9f10
OlenaSekreta <72521407+OlenaSekreta@users.noreply.github.com>
2021-05-13 12:56:58 +0300

    Add files via upload

---
+++ КР3Task3main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "ÊÐ2Task3.h"
+using namespace std;
+
+int main() {
+	Triangle triangle;
+	cout << "input sides: ";
+	cin >> triangle.Side1;
+	cin >> triangle.Side2;
+	cin >> triangle.Side3;
+	triangle.Perimeter();
+
+}
